4c5258958cca575ae447b4e7c1927aba89dc5f22
Rajesh10 <guddewadi10@gmail.com>
2024-02-05 22:59:23 +0530

    Functional Interface

--- src/InterF.java
+++ src/InterF.java
@@ -1,12 +1,28 @@
 @FunctionalInterface
 public interface InterF {
-    public abstract void m();
+    public abstract String m(String s);
 
     default void m2(){
-
+        System.out.println("Guddewadi");
     }
 
-    static void m3(){
+     static void m3(){
+        System.out.println("Rajkumar");
+    }
+}
 
+@FunctionalInterface
+interface b extends InterF{
+    public abstract String m(String s);
+}
+class test{
+    public static void main(String[] args) {
+        String a ="Raj";
+        b f = (String s) -> s+" Guddewadi";
+        String ans = f.m(a);
+        System.out.println(ans);
+
+        f.m2();
+        InterF.m3();
     }
 }
